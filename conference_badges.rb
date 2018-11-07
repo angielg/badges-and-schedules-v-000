51401b53241f28af81a1fb1a6b96362d0bc6cdb6
Angie Goza <angielg9888@gmail.com>
2018-11-07 15:43:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_message = [ ]
+  names.each do |name|
+  badge_message << name
+  end
+  return badge_message
+    
+end
