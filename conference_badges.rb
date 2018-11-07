c4afe494a6ee05af49688d735d94469cf2825f54
Angie Goza <angielg9888@gmail.com>
2018-11-07 16:24:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badges = [ ]
   attendees.each do |name|
-  badge_maker(name)
+  badge_maker(name) = badge
+  badges << badge
   end
 end
